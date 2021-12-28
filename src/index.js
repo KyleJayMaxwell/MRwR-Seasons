07c357126794b23e78ba61e5c2217f8a185224e5
Kyle Maxwell <kyle.maxwell@Kyle-Maxwells-MacBook-Pro.local>
2021-12-27 19:37:19 -0800

    [Seasons] Conditionally rendering content, end of section 5

--- src/index.js
+++ src/index.js
@@ -20,13 +20,14 @@ class App extends React.Component {
 
     // Have to define render
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-        );
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+        return <div>Loading</div>;
     }
 }
 
